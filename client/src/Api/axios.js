120f88958535fec894e389b0ad7d76b2d3f74cc6
David200307 <juandavidfernandeztriana@gmail.com>
2024-12-09 17:16:40 -0500

    hosting 4

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+// Usar la URL de producción si estamos en producción, de lo contrario usar localhost
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    withCredentials: true
-})
+    baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://proyecto-back-d1m5.onrender.com/api'  // URL del backend en producción
+        : 'http://localhost:3000/api',  // URL del backend en desarrollo
+    withCredentials: true  // Para manejar cookies, si es necesario
+});
 
-export default instance
+export default instance;
