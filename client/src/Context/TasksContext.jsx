9457746718e639ce28f0f4ea50f25ac1db9526cc
David200307 <juandavidfernandeztriana@gmail.com>
2024-12-09 19:23:25 -0500

    asfsgsdfgkj

--- client/src/Context/TasksContext.jsx
+++ client/src/Context/TasksContext.jsx
@@ -62,14 +62,22 @@ const getTask = async (id) =>{
     }
 }
 
-const updateTask = async (id, task) =>{
-try {
-    await updateTaskRequest(id, task)
-    
-} catch (error) {
-    console.log(error)
-}
-}
+const updateTask = async (id, updatedTask) => {
+    try {
+      const res = await updateTaskRequest(id, updatedTask); // Solicitud al backend para actualizar la tarea
+  
+      if (res.status === 200) {
+        // Si la tarea se actualiza correctamente, actualiza el estado en el frontend
+        setTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task._id === id ? { ...task, ...updatedTask } : task
+          )
+        );
+      }
+    } catch (error) {
+      console.log("Error updating task:", error);
+    }
+  };
     return(
         <TaskContext.Provider value = {{
             tasks,
